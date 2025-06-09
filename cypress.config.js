15a1b19a18b3b2640873a080a1c4b270fd3881f1
alegullo <alexandre.gullo@hotmail.com>
2025-06-09 13:56:13 -0300

    test1

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,17 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 5000,
   viewportWidth: 1920,
   viewportHeight: 1080,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  }
+    baseUrl: "https://erickwendel.github.io/vanilla-js-web-app-example/",
+
+    //nao vai limpar a tela apos cada teste
+    testIsolation: false,
+  },
 });
